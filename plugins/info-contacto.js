df5fcfaaa3ec187bec3c545e2e35621b707a083d
Eliasivan <159085113+Eliasivan@users.noreply.github.com>
2024-03-06 15:26:25 -0300

    Actualizar info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -12,7 +12,7 @@ let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
 [devnum, `${await conn.getName(''+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇵🇾 Paraguay`, ``, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 LobitoBot-MD`, `📵 No Hacer Spam`, , `🇵🇾 Paraguay`,email `LobitoBot-MD`, biobot]
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 LobitoBot-MD`, `📵 No Hacer Spam`, , `🇵🇾 Paraguay`email, `LobitoBot-MD`, biobot]
 ], m)
   
 }
