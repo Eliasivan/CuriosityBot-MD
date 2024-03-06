79f70db73216885eaa6b054c497a3a0e805914af
Eliasivan <159085113+Eliasivan@users.noreply.github.com>
2024-03-06 15:02:53 -0300

    Actualizar info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -6,7 +6,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let pp = await conn.profilePictureUrl(who).catch(_ => '')
 let name = await conn.getName(who)
 let biografia = await conn.fetchStatus('595992809980' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('1(505)814-5540' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('1508145540' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
