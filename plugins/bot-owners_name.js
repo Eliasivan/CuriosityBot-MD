4b76a8184307305822c4f1625e1bc6275d8876fa
Eliasivan <159085113+Eliasivan@users.noreply.github.com>
2024-03-05 22:38:02 -0300

    Actualizar bot-owners_name.js

--- plugins/bot-owners_name.js
+++ plugins/bot-owners_name.js
@@ -8,7 +8,7 @@ var chat = global.db.data.chats[m.chat]
 
 if (/^Azami|azami|Zam|zam$/i.test(m.text) && !chat.isBanned) { 
 
-let GB = `🚩 *Parece que se ha mencionado el nombre de mi creador* ( @59162714296 )`
+let GB = `🚩 *Parece que se ha mencionado el nombre de mi creador* ( @595972157130 )`
 
 m.reply(GB, m.chat, { mentions: conn.parseMention(GB)})
 
@@ -17,7 +17,7 @@ m.reply(GB, m.chat, { mentions: conn.parseMention(GB)})
 {
 if (/^kitsune|arumi|ari|aruu$/i.test(m.text) && !chat.isBanned) { 
 
-let KIT = `🚩 *Mencionaste el nombre de un colaborador* ( @5215610314499 )`
+let KIT = `🚩 *Mencionaste el nombre de un colaborador* ( @593939005387 )`
 
 m.reply(KIT, m.chat, { mentions: conn.parseMention(KIT)})
 }
