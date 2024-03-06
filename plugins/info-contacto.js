88e861f9260d114c1f601d228c64bc346d1ff79c
Eliasivan <159085113+Eliasivan@users.noreply.github.com>
2024-03-06 14:59:18 -0300

    Actualizar info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -5,13 +5,13 @@ var handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => '')
 let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografia = await conn.fetchStatus('595992809980' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('1(505)814-5540' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('595992809980'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇵🇾 Paraguay`, `https://www.youtube.com/@Azami_YT`, bio],
+[devnum, `${await conn.getName('595992809980'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇵🇾 Paraguay`, ``, bio],
 [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 LobitoBot-MD`, `📵 No Hacer Spam`, email, `🇵🇾 Paraguay`, `LobitoBot-MD`, biobot]
 ], m)
   
